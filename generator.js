6c02b6c0c79c7af36de672b37dfc8508015d7fae
carwest <carwestsam@gmail.com>
2017-11-15 23:01:26 +0800

    first undo

--- generator.js
+++ generator.js
@@ -1,6 +1,50 @@
-class Generator {
-  constructor (origin) {
+let shm = undefined
+class StateHistoryMgr {
+  constructor () {
+    this.history = []
+    this.origin = {}
+  }
+  static getInstance () {
+    if (typeof shm == 'undefined') {
+      shm = new StateHistoryMgr()
+    }
+    return shm
+  }
+  addRawMutation(key, raw) {
+    this.origin[key] = raw
+  }
+
+  append(key, args) {
+    this.history.push({
+      mutation: key,
+      args
+    })
+  }
+
+  undo () {
+    let {mutation, args} = this.history[0]
+    this.origin[mutation]().backward.apply(null, args)
   }
 }
+shm = new StateHistoryMgr()
+class VuexConfigGenerator {
+  // origin = {}
+  constructor (origin) {
+    this.origin = origin
+  }
+  attachMutations (attachObject) {
+    for ( let key in attachObject ){
+      let func = attachObject[key]     
+      let shm = StateHistoryMgr.getInstance()
+      shm.addRawMutation(key, func)
 
-module.exports = Generator
+      this.origin.mutations[key] = function() {
+        shm.append(key, arguments)
+        func().forward.apply(null, arguments)
+      }
+    }
+  }
+
+}
+
+module.exports = {VuexConfigGenerator, StateHistoryMgr}
--- generator.test.js
+++ generator.test.js
@@ -1,6 +1,9 @@
 const expect = require('chai').expect
 const Vue = require('vue')
 const Vuex = require('vuex')
+const {VuexConfigGenerator, StateHistoryMgr} = require('./generator')
+let shm = StateHistoryMgr.getInstance()
+
 Vue.use(Vuex)
 
 let OriginVuexDes = {
@@ -19,11 +22,35 @@ let OriginVuexDes = {
 describe("Test Generator working properly", function() {
   describe("Should create correct vuex object", function() {
     it('first test', function() {
-      expect(true).to.be.eq(true);  
+      expect(true).to.be.eq(true)
     })
     it('create native vuex object', function() {
-      $store = new Vuex.Store(OriginVuexDes);
+      $store = new Vuex.Store(OriginVuexDes)
       expect(0).to.be.eq($store.getters.o_count)
+    })
+  })
+  describe("Should have undo operation", function () {
+    GeneratedVuexDes = new VuexConfigGenerator(OriginVuexDes).attachMutations({
+      addCount: function () {
+        return {
+          forward: function (state, number) {
+            state.count += number
+          },
+          backward: function (state, number) {
+            state.count -= number
+          }
+        }
+      }
+    })
+    $store = new Vuex.Store(OriginVuexDes)
+
+    it('1, going forword', function() {
+      $store.commit('addCount', 10)
+      expect($store.getters.o_count).to.be.eq(10)
+    })
+    it('2, undo previous job', function () {
+      shm.undo()
+      expect($store.getters.o_count).to.be.eq(0)
     })
   })
 })
