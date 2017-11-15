0b31f6bbc51d9b479ea1984e14821a707194e51e
carwest <carwestsam@gmail.com>
2017-11-15 17:05:41 +0800

    init test

---
+++ generator.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+
+describe("Test Generator working properly", function() {
+  describe("Should create correct vuex object", function() {
+    it('first test', function() {
+      expect(true).to.be.eq(true);  
+    })
+  })
+})
